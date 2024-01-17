9c54ae7b130b549761d0937311d2c81211ce34ea
thanhhnguyen23 <thanh.h.nguyen23@gmail.com>
2024-01-17 18:10:09 -0500

    add child routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { ProjCmpComponent } from './proj-cmp/proj-cmp.component';
+import { RpCmpComponent } from './rp-cmp/rp-cmp.component';
+import { HomeCmpComponent } from './home-cmp/home-cmp.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeCmpComponent },
+  {
+    path: 'rre',
+    children: [
+      // * NOTE: http://localhost:4200/rre/resource-pools
+      { path: 'resource-pools', component: RpCmpComponent },
+      { path: '', redirectTo: 'projects', pathMatch: 'full' },
+
+      // * NOTE: http://localhost:4200/rre/projects
+      { path: 'projects', component: ProjCmpComponent },
+      { path: '**', component: RpCmpComponent },
+    ],
+  },
+  { path: '**', component: HomeCmpComponent },
+];
